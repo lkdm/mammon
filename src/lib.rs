e7eb61aae7564546c6681402cf16a9eb46097efc
Luke M <44519853+lkdm@users.noreply.github.com>
2024-09-08 07:30:31 +1000

    feat: rename money to cent.

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,24 @@ trait MoneyOps<T> {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Money<T: PrimInt + FromPrimitive + ToPrimitive>(T);
+struct Cent<T: PrimInt + FromPrimitive + ToPrimitive>(T);
+
+impl<T: PrimInt + FromPrimitive> From<f32> for Cent<T> {
+    fn from(f: f32) -> Self {
+        // Multiply by 100 and round to the nearest cent
+        let rounded = (f * 100.0).round();
+        let cents = T::from_f32(rounded).expect("Failed to convert f32 to T");
+        Cent::new(cents)
+    }
+}
+impl<T: PrimInt + FromPrimitive> From<f64> for Cent<T> {
+    fn from(f: f64) -> Self {
+        // Multiply by 100 and round to the nearest cent
+        let rounded = (f * 100.0).round();
+        let cents = T::from_f64(rounded).expect("Failed to convert f64 to T");
+        Cent::new(cents)
+    }
+}
 
 // impl<T: PrimInt + FromPrimitive + ToPrimitive> MoneyOps<Money<T>> for Money<T> {
 //     fn parse_string(s: &str) -> T {
@@ -45,79 +62,90 @@ struct Money<T: PrimInt + FromPrimitive + ToPrimitive>(T);
 //     }
 // }
 
-impl<T: PrimInt + FromPrimitive + ToPrimitive> Money<T> {
+impl<T: PrimInt + FromPrimitive + ToPrimitive> Cent<T> {
     pub fn new(value: T) -> Self {
-        Money(value)
+        Cent(value)
     }
     fn div_round_up(a: T, b: T) -> T {
         (a + b - T::one()) / b
     }
 }
 
-impl<T: PrimInt + FromPrimitive + ToPrimitive> Add for Money<T> {
+impl<T: PrimInt + FromPrimitive + ToPrimitive> Add for Cent<T> {
     type Output = Self;
     fn add(self, rhs: Self) -> Self::Output {
-        Money(self.0 + rhs.0)
+        Cent(self.0 + rhs.0)
     }
 }
 
-impl<T: PrimInt + FromPrimitive + ToPrimitive> Sub for Money<T> {
+impl<T: PrimInt + FromPrimitive + ToPrimitive> Sub for Cent<T> {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
-        Money(self.0 - rhs.0)
+        Cent(self.0 - rhs.0)
     }
 }
 
-impl<T: PrimInt + FromPrimitive + ToPrimitive> Mul for Money<T> {
+impl<T: PrimInt + FromPrimitive + ToPrimitive> Mul for Cent<T> {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
         let hundred = T::from_u8(100).unwrap();
         let result = Self::div_round_up(self.0 * rhs.0, hundred);
-        Money(result)
+        Cent(result)
     }
 }
 
-impl<T: PrimInt + FromPrimitive + ToPrimitive> Div for Money<T> {
+impl<T: PrimInt + FromPrimitive + ToPrimitive> Div for Cent<T> {
     type Output = Self;
     fn div(self, rhs: Self) -> Self::Output {
         let hundred = T::from_u8(100).unwrap();
         let result = Self::div_round_up(self.0 * hundred, rhs.0);
-        Money(result)
+        Cent(result)
     }
 }
 
-impl<T: PrimInt + FromPrimitive + ToPrimitive> Rem for Money<T> {
+impl<T: PrimInt + FromPrimitive + ToPrimitive> Rem for Cent<T> {
     type Output = Self;
     fn rem(self, rhs: Self) -> Self::Output {
-        Money(self.0 % rhs.0)
+        Cent(self.0 % rhs.0)
     }
 }
 
-pub type Moneyi128 = Money<i128>;
-pub type Moneyisize = Money<isize>;
-pub type Moneyi64 = Money<i64>;
-pub type Moneyi32 = Money<i32>;
-pub type Moneyi16 = Money<i16>;
-pub type Moneyi8 = Money<i8>;
-pub type Moneyusize = Money<usize>;
-pub type Moneyu128 = Money<u128>;
-pub type Moneyu64 = Money<u64>;
-pub type Moneyu32 = Money<u32>;
-pub type Moneyu16 = Money<u16>;
-pub type Moneyu8 = Money<u8>;
+pub type Centi128 = Cent<i128>;
+pub type Centisize = Cent<isize>;
+pub type Centi64 = Cent<i64>;
+pub type Centi32 = Cent<i32>;
+pub type Centi16 = Cent<i16>;
+pub type Centi8 = Cent<i8>;
+pub type Centusize = Cent<usize>;
+pub type Centu128 = Cent<u128>;
+pub type Centu64 = Cent<u64>;
+pub type Centu32 = Cent<u32>;
+pub type Centu16 = Cent<u16>;
+pub type Centu8 = Cent<u8>;
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn test_money() {
-        let m1 = Moneyi32::new(123);
-        let m2 = Moneyi32::new(456);
-        assert_eq!(m1 + m2, Money(579));
-        assert_eq!(m1 - m2, Money(-333));
-        assert_eq!(m1 * m2, Money(561));
-        assert_eq!(m1 / m2, Money(27));
-        assert_eq!(m1 % m2, Money(123));
+        let m1 = Centi32::new(123);
+        let m2 = Centi32::new(456);
+        assert_eq!(m1 + m2, Cent(579));
+        assert_eq!(m1 - m2, Cent(-333));
+        assert_eq!(m1 * m2, Cent(561));
+        assert_eq!(m1 / m2, Cent(27));
+        assert_eq!(m1 % m2, Cent(123));
+    }
+
+    #[test]
+    fn test_int_interop() {
+        let m1 = Centi32::new(123);
+        let n1: f32 = 4.56;
+        assert_eq!(m1 + n1.into(), Cent(579));
+        assert_eq!(m1 - n1.into(), Cent(-333));
+        assert_eq!(m1 * n1.into(), Cent(561));
+        assert_eq!(m1 / n1.into(), Cent(27));
+        assert_eq!(m1 % n1.into(), Cent(123));
     }
 }
 
